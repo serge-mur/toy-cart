f2b4e30bf0e31946864d2a9fefe920e7b29015d6
serge-mur <serge-mur@yandex.ru>
2021-05-05 13:59:32 +0300

    verstka

--- js/script.js
+++ js/script.js
@@ -10,4 +10,14 @@ $(document).ready(function() {
         }
     });
 
+    // blue border active payment
+    $('.payment-block .form-check-input:checked').closest('.form-check-label').toggleClass('active');
+    $('.payment-block .form-check-input').change(function() {
+        if ($(!this.checked)) {
+            $(this).closest('.payment-block').find('.form-check-label').removeClass('active');
+            $(this).closest('.form-check-label').toggleClass('active');
+            console.log(this.value);
+        }
+    });
+
 }); //document ready end
